0e559c286f37d9391663bf105598ead395fe1362
jpacsai <judit.pacsai@gmail.com>
2019-07-25 15:28:05 +0200

    connect to mongoDB

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect db
+connectDB();
 
 app.get('/', (req, res) => res.send('API running...'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`...Server started on port ${PORT}`));

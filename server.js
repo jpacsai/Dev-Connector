a99fd28530c3d0db0f57c8091d67424a0d00a4a6
jpacsai <judit.pacsai@gmail.com>
2019-07-25 16:23:18 +0200

    add basic routes

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+// Import routes
+const auth = require('./routes/api/auth');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+const users = require('./routes/api/users');
+
 const app = express();
 
 // Connect db
 connectDB();
 
 app.get('/', (req, res) => res.send('API running...'));
+
+// Define routes
+app.use('/api/auth', auth);
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
+app.use('/api/users', users);
 
 const PORT = process.env.PORT || 5000;
 

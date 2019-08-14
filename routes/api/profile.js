c428e920c938c43142a2be7d8ca02a19b2fd6b1b
jpacsai <judit.pacsai@gmail.com>
2019-08-14 16:20:40 +0200

    add route - PUT api/profile/education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -210,4 +210,47 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
   }
 });
 
+// @route    PUT api/profile/education
+// @descr    Add profile education
+// @access   Private
+router.put('/education', [ auth, [
+  check('school', 'School is required').not().isEmpty(),
+  check('certificate', 'Certificate is required').not().isEmpty(),
+  check('from', 'From date is required').not().isEmpty(),
+  check('field_of_study', 'Field of study date is required').not().isEmpty(),
+]], async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
+  const {
+    school,
+    certificate,
+    from,
+    field_of_study,
+    description
+  } = req.body;
+
+  const newEdu = {
+    school,
+    certificate,
+    from,
+    field_of_study,
+    description
+  }
+
+  try {
+    const profile = await Profile.findOneAndUpdate(
+      { user: req.user.id },
+      { $push: { education: { $each: [newEdu], $position: 0 } } },
+      { new: true }
+    );
+    if (!profile) return res.status(400).json({ msg: 'Profile not found'});
+    res.json(profile);
+    
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
